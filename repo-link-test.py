86a5073f3a2293c711747a360d5e6bb3e9046523
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2020-02-03 20:55:18 +0100

    Working on ability to clone repos through python, essentially just calls a child process

--- repo-link-test.py
+++ repo-link-test.py
@@ -1,10 +1,20 @@
 import re
 import http
+from os.path import join
+from os import getcwd, listdir
 from sys import argv
+from subprocess import run
 # Make the regex for the URI
-url_match = re.compile("")
+url_match = re.compile("https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)")
 
 # Get url of the repo to pull from 
 repo = argv[1]
 
+# clone the input repo 
+run(args=["git", "clone", repo, "repo/"])
 
+files = listdir("repo")
+
+print(files)
+
+matches = []
