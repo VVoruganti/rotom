01ba654071f65616e8e5414dc5ca53b1fc027e37
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2020-03-05 15:53:49 +0100

    Setup Repo
    Adding stuff for a more conventional git repo such as the ignore
    file and the readme. Also added a requirements file for python and
    started using virtual environments
    
    In the actual project implemented the recursive step

---
+++ README.md
@@ -0,0 +1,7 @@
+# Git Repository URL Tester
+
+This project is being made in response to the fact that many old projects contain dead links
+this tool will make it easy to quickly check all of the links in a repository to 
+find the location of any/all links and determine if they are dead or not. 
+
+
--- repo-link-test.py
+++ repo-link-test.py
@@ -1,36 +1,58 @@
 import re
 import http
-from os.path import join, isfile
-from os import getcwd, listdir
+from os.path import join, isfile, isdir, dirname, abspath
+from os import listdir
 from sys import argv
 from subprocess import run
 # Make the regex for the URI
+# Sourced this regex from https://ihateregex.io
 url_match = re.compile(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)')
 
+curdir = dirname(abspath(__file__))
 # Get url of the repo to pull from 
 # clone the input repo 
 if(len(argv) > 1):
     repo = argv[1]
-    run(args=["rf", "-rf", "repo"])
-    run(args=["git", "clone", repo, "repo/"])
-    
-files = listdir("repo")
-print(files)
+    if(isdir(join(curdir,"test-repo"))):
+        run(args=["rf", "-rf", "test-repo"])
+    run(args=["git", "clone", repo, "test-repo/"])
+# TODO look into possible cloning it to the /tmp directory and cleaning it at the end 
 
+#files = listdir("test-repo")
+#print(files)
+#print(len(files))
 matches = []
 
-for filename in files:
-    file_path = join("repo", filename)
-    if(isfile(file_path)): # Check to make sure it is not a directory
-        file = open(file_path, "r")
-        text = " ".join(file.readlines())
-        match = re.search(url_match,text)
-        if(match != None):
-            print(match)
-    else:
-        print("test")
+# for filename in files:
+#     file_path = join(curdir, "test-repo", filename)
+#     if(isfile(file_path)): # Check to make sure it is not a directory
+#         file = open(file_path, "r")
+#         text = " ".join(file.readlines())
+#         match = re.search(url_match,text)
+#         if(match != None):
+#             print(match)
+#     else:
+#         recursive_search(filename)
+#         print("test")
         # need a recursive step here
 
-def recursive_search(files):
+def recursive_search(directory):
+    global url_match
+    global matches
+    files = listdir(directory)
     print(files)
+    for filename in files:
+        file_path = join(directory, filename)
+        if(isfile(file_path)):
+            with open(file_path, "r") as file:
+                text = " ".join(file.readlines())
+                match = re.search(url_match,text)
+                if(match != None):
+                    print(match)
+                    matches.append(match)
+        else:
+            recursive_search(file_path)
 
+recursive_search(join(curdir,"test-repo"))
+# have a final output that looks similar to how rip grep organizes its output sampel command below
+# rg -e https:\/\/
